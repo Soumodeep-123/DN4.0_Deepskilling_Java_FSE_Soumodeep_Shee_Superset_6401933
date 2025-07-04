f434add5b2ed09c6c8533b3a82d7ba6a124abbd4
Soumodeep-123 <120673851+Soumodeep-123@users.noreply.github.com>
2025-07-04 23:40:56 +0530

    Add files via upload

---
+++ Week_3_Assignment/Spring_Core_Maven/Exercise_1_Configuring_a_Basic_Spring_Application/librarymanagement/src/main/java/com/library/Test/TimerTester.java
@@ -0,0 +1,13 @@
+package com.library.Test;
+
+public class TimerTester {
+	
+	public void func() {
+		System.out.println("TimerTester: Doing some work...");
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("Processing item " + (i + 1));
+        }
+	}
+	
+}
---
+++ Week_3_Assignment/Spring_Core_Maven/Exercise_1_Configuring_a_Basic_Spring_Application/librarymanagement/src/main/java/com/library/repository/BookRepository.java
@@ -0,0 +1,13 @@
+package com.library.repository;
+
+
+
+public class BookRepository {
+    public BookRepository() {
+        System.out.println("BookRepository created");
+    }
+    
+    public void getBook() {
+        System.out.println(" Fetching book from repository...");
+    }
+}
