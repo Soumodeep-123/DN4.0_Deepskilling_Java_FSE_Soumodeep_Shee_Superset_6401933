b2ebb29287fcaeac8681a61fc462ee5b9fa895bf
Soumodeep-123 <120673851+Soumodeep-123@users.noreply.github.com>
2025-07-04 23:59:08 +0530

    Add files via upload

---
+++ Week_3_Assignment/Spring_Core_Maven/Exercise_5_Configuring_the_Spring_IoC_Container/librarymanagement/src/main/java/com/library/Service/BookService.java
@@ -0,0 +1,16 @@
+package com.library.Service;
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+    private BookRepository bookRepository;
+    
+    public void setBookRepository(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+    
+    public void issueBook(){
+        System.out.println("Book issued");
+        bookRepository.getBook();
+    }
+}
