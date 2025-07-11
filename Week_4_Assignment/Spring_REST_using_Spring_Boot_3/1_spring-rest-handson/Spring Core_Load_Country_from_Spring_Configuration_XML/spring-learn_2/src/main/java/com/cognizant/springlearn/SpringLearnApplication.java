cd9fc28587ad7e35dfc9a863b22f8ae75d4b9655
Soumodeep-123 <120673851+Soumodeep-123@users.noreply.github.com>
2025-07-11 10:02:57 +0530

    codes

---
+++ Week_4_Assignment/Spring_REST_using_Spring_Boot_3/1_spring-rest-handson/Spring Core_Load_Country_from_Spring_Configuration_XML/spring-learn_2/src/main/java/com/cognizant/springlearn/SpringLearnApplication.java
@@ -0,0 +1,42 @@
+package com.cognizant.springlearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+
+@SpringBootApplication
+public class SpringLearnApplication {
+
+	private static final Logger LOGGER = LoggerFactory.getLogger(SpringLearnApplication.class);
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLearnApplication.class, args);
+		System.out.println();
+		System.out.println("___________________________________________________");
+		System.out.println();
+		LOGGER.info("Inside main method");
+		System.out.println("Inside main method");
+		System.out.println("The date is ");
+		displayDate();
+	}
+
+	public static void displayDate() {
+		ApplicationContext context = new ClassPathXmlApplicationContext("date-format.xml");
+		SimpleDateFormat format = context.getBean("dateFormat", SimpleDateFormat.class);
+		try {
+			Date date = format.parse("31/12/2018");
+			System.out.println(date);
+		} catch (ParseException e) {
+			e.printStackTrace();
+		}
+	}
+
+}
